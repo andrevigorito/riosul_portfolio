abc37386ddeaa9dd73758101bb018db6ed64b7ab
romeroalmeida <romerobm@gmail.com>
2019-07-08 16:10:00 -0300

    Operacional

--- src/app/views/Operacional.js
+++ src/app/views/Operacional.js
@@ -1,13 +1,32 @@
 import React, {Component} from 'react'
+import API from '../service/api';
+
 // Images
 import iconOperacional from '../img/icons/title-ope.png'
 
 // Components
-
+import Loading from './components/Loading';
 
 
 class Operacional extends Component {
-
+    state = {
+        operacional: [],
+        isLoading: false,
+    }
+    
+    componentDidMount() {
+		this.setState({ isLoading: true })
+		API.get(`products`)
+		  .then(res => {
+			const operacional = res.data;
+			console.log(operacional)
+			this.setState({ 
+				operacional,
+				isLoading: false
+			 });
+			
+		  })
+      }
 	
     render(){
         return(
@@ -21,6 +40,29 @@ class Operacional extends Component {
                         </h1>
                     </div>
 
+                    <div className='list-ope'>
+                        <header className="header-list-ope">
+                            <p>Crit.</p>
+                            <p>PO</p>
+                            <p>Produto</p>
+                            <p>Descrição</p>
+                            <p>Qtd.</p>
+                            <p>Planta Destino</p>
+                            <p>Status</p>
+                        </header>
+                        { this.state.isLoading && <Loading /> }
+                        { this.state.operacional.map(ope => 
+                            <div className='item'>
+                                <span className='critico'></span>
+                                <p className='po'>-</p>
+                                <p className='produto'>{ope.product_id}</p>
+                                <p className='descricao'>{ope.product_description}</p>
+                                <p className='qtd'>-</p>
+                                <p className='pd'>-</p>
+                                <p className='status alert'>Possível Atraso</p> 
+                            </div>
+                        )}
+                    </div>
                     
 					
 				</div>
