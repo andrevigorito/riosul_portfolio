364f080c719c9502e6800fa3954a0e6c64eae428
romeroalmeida <romerobm@gmail.com>
2019-08-02 15:37:45 -0300

    Update Operacional.js

--- src/app/views/Operacional.js
+++ src/app/views/Operacional.js
@@ -34,7 +34,7 @@ class Operacional extends Component {
   async componentDidMount() {
     this.setState({ isLoading: true });
 
-    const response = await API.get('poItems/1');
+    const response = await API.get('poItems/pag/1');
     const { data: operacional } = response;
 
     this.setState({
